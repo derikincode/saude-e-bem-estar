5939cd4d560ec84047e3c173cd9142fb01064309
derikincode <derikoliveira252@gmail.com>
2024-05-29 13:57:39 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [htmlCode, setHtmlCode] = useState('');
+  const [cssCode, setCssCode] = useState('');
+  const [jsCode, setJsCode] = useState('');
+
+  const handleHtmlChange = (event) => {
+    setHtmlCode(event.target.value);
+  };
+
+  const handleCssChange = (event) => {
+    setCssCode(event.target.value);
+  };
+
+  const handleJsChange = (event) => {
+    setJsCode(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Code-container">
+        <div className="Code-input-container">
+        <div class="TitleCode">
+        <label class="html" htmlFor="html-input">HTML</label>
+        </div>
+          <textarea
+            className="Code-input"
+            value={htmlCode}
+            onChange={handleHtmlChange}
+            placeholder="Digite seu código HTML aqui..."
+          />
+        </div>
+        <div className="Code-input-container">
+        <div class="TitleCode">
+        <label class="css" htmlFor="html-input">CSS</label>
+        </div>
+          <textarea
+            className="Code-input"
+            value={cssCode}
+            onChange={handleCssChange}
+            placeholder="Digite seu código CSS aqui..."
+          />
+        </div>
+        <div className="Code-input-container">
+        <div class="TitleCode">
+        <label class="js" htmlFor="html-input">JS</label>
+        </div>
+          <textarea
+            className="Code-input"
+            value={jsCode}
+            onChange={handleJsChange}
+            placeholder="Digite seu código JavaScript aqui..."
+          />
+        </div>
+      </div>
+      <div className="Preview">
+        <style>{cssCode}</style>
+        <script>{jsCode}</script>
+        <div dangerouslySetInnerHTML={{ __html: htmlCode }} />
+      </div>
     </div>
   );
 }
